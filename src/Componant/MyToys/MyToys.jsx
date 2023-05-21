0fc2e52de2eb65da2c2bf676a4187192abbcbb9c
yahia masud <yahiamasud974@gmail.com>
2023-05-21 19:08:21 +0600

    sort add

--- src/Componant/MyToys/MyToys.jsx
+++ src/Componant/MyToys/MyToys.jsx
@@ -7,10 +7,10 @@ const MyToys = () => {
 
     const { user } = useContext(AuthContext)
     const [myToy, setMyToy] = useState([])
-    const [control, setControl]= useState(false)
+    const [control, setControl] = useState(false)
 
 
-// data loading..............................
+    // data loading..............................
 
     const url = `https://assingment-11-serversit-yahiamasud.vercel.app/myToy?email=${user?.email}`
     useEffect(() => {
@@ -21,30 +21,50 @@ const MyToys = () => {
                 setMyToy(data)
                 setControl(!control);
             })
-    }, [url,control])
-    
-// deleted.......................
-    const handleDalate = id =>{
+    }, [url, control])
+
+    // deleted.......................
+    const handleDalate = id => {
         const proceed = confirm("are you sure you want to dataled");
-        if(proceed){
-            fetch(`https://assingment-11-serversit-yahiamasud.vercel.app/toyCar/${id}`,{
-                method:"DELETE"
+        if (proceed) {
+            fetch(`https://assingment-11-serversit-yahiamasud.vercel.app/toyCar/${id}`, {
+                method: "DELETE"
             })
-            .then(res=>res.json())
-            .then((data) =>{
-                if (data.deletedCount > 0){
-                    const remaining = toyCar.filter(toyCar._id !== id);
-                    setMyToy(remaining);
-                    setControl(!control);
-                    
-                }
-            })
+                .then(res => res.json())
+                .then((data) => {
+                    if (data.deletedCount > 0) {
+                        const remaining = toyCar.filter(toyCar._id !== id);
+                        setMyToy(remaining);
+                        setControl(!control);
+
+                    }
+                })
         }
     }
+    // sort data funtion
+    const ascendingEvent = () => {
+        let data = [...myToy]
+        if (data.length > 0) {
+            let result = data.sort((a, b) => a.price.localeCompare(b.price))
+            setMyToy(result)
+
+        }
+    }
+    const descendingEvent = () => {
+        let data = [...myToy]
+        if (data.length > 0) {
+            let result = data.sort((a, b) => b.price.localeCompare(a.price))
+            setMyToy(result)
+
+        }
+    }
+
+
 
     return (
         <div className="container mx-auto p-20">
             <h1 className="text-center text-white font-bold text-5xl m-5 p-4 ">My Post toyCar </h1>
+            <div className='mb-5'><button className='btn btn-primary' onClick={ascendingEvent} >ascending</button> <button onClick={descendingEvent} className='btn btn-primary'>descending</button></div>
             <table striped bordered hover className=" text-center text-white w-full">
                 <thead className=''>
                     <tr className='border-b-2'  >
@@ -59,7 +79,7 @@ const MyToys = () => {
                     </tr>
                 </thead>
                 <tbody className=''>
-                    {myToy?.map((myTo , index )=><MyTable index={index} myTo={myTo}  handleDalate = {handleDalate}></MyTable>)}
+                    {myToy.map((myTo, index) => <MyTable index={index} myTo={myTo} handleDalate={handleDalate}></MyTable>)}
                 </tbody>
             </table>
         </div>
